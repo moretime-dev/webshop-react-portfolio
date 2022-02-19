dac292ed6755c51471abb21a9a8dba97c4cc39d9
moretime-dev <moretimeproductions@googlemail.com>
2022-02-19 09:34:35 +0100

    user sign up works

--- src/pages/AddNewUser.js
+++ src/pages/AddNewUser.js
@@ -1,8 +1,19 @@
 import SignUpForm from "../components/Users/SignUpForm";
 
+import { db } from "../firebase_config";
+import { collection, addDoc } from "firebase/firestore";
+
+const usersCollection = collection(db, "users");
 const AddNewUser = () => {
-  const onSignUpFormSubmitHandler = (userData) => {
+  const onSignUpFormSubmitHandler = async (userData) => {
     console.log(userData);
+
+    const user = {
+      userEmail: userData.userEmail,
+      password: userData.userPassword,
+      role: "user",
+    };
+    await addDoc(usersCollection, user);
   };
 
   return <SignUpForm onPassUserData={onSignUpFormSubmitHandler} />;
